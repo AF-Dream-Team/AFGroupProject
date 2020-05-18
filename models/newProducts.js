9de9a5c2ff207390da54a1e631760e9947a05c30
Sawandi <sawandiudeshitha1997@gmail.com>
2020-05-19 01:47:07 +0530

    Updated properties in newProduct.js file

--- models/newProducts.js
+++ models/newProducts.js
@@ -3,10 +3,11 @@ const mongoose = require('mongoose')
 var NewProduct = mongoose.model('product',{
     name : {type:String},
     category : {type:String},
-    decription : {type:String},
+    description : {type:String},
     quantity : {type:Number},
     price : {type:Number},
-    discount : {type:Number}
+    discount : {type:Number},
+    discountMonth : {type:String}
 })
 
 module.exports = { NewProduct }
