c5dc77b47c2288c5dadebed7681e918937ec543b
ranruwini <ranruwini@gmail.com>
2020-05-21 21:08:53 +0530

    register user added

--- stack-client/src/actions/api.js
+++ stack-client/src/actions/api.js
@@ -12,12 +12,31 @@ export default {
             delete: id => axios.delete(url + id)
         }
     },
+	 message(url = baseUrl + 'message/') {
+        return {
+            fetchAll: () => axios.get(url),
+            fetchById: id => axios.get(url + id),
+            create: newRecord => axios.post(url, newRecord),
+            update: (id, updatedRecord) => axios.put(url + id, updatedRecord),
+            delete: id => axios.delete(url + id)
+        }
+    },
 
     product(url = baseUrl + 'product/') {
         return {
             fetchAll: () => axios.get(url),
             fetchById: id => axios.get(url + id),
             create: newRecord => axios.post(url, newRecord),
+            update: (id, updatedRecord) => axios.put(url + id, updatedRecord),
+            delete: id => axios.delete(url + id)
+        }
+    },
+	    registerUser(url = baseUrl + 'register/') {
+        return {
+            fetchAll: () => axios.get(url),
+            fetchById: id => axios.get(url + id),
+            create: newRecord => axios.post(url, newRecord),
+            email: msg => axios.post(url+"/email", msg),
             update: (id, updatedRecord) => axios.put(url + id, updatedRecord),
             delete: id => axios.delete(url + id)
         }
