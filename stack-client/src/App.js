db4ff4c3f2b697b1cfba05d7ceaa80e951f19a29
Sawandi <sawandiudeshitha1997@gmail.com>
2020-05-21 11:21:42 +0530

    done productList in client side

--- stack-client/src/App.js
+++ stack-client/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/nav';
-import Login from './components/login';
-import Register from './components/register';
 import Order from './components/order';
 import Product from './components/product';
-import Users from './components/usersList';
-import Category from './components/category';
+import ProductList from './components/productList';
 import { BrowserRouter as Router , Switch , Route } from 'react-router-dom';
 import ButterToast,{ POS_RIGHT,POS_TOP } from "butter-toast";
 
@@ -20,9 +17,9 @@ class App extends React.Component {
             <Nav />
             <Switch>
               <Route path="/order" component={ Order }></Route>
-              <Route path="/usersList" component={ Users }></Route>
-              <Route path="/category" component={ Category }></Route>
               <Route path="/product" component={ Product }></Route>
+			   <Route path="/productList" component={ ProductList }></Route>
+              <Route path="/" component={ ProductList }></Route>
             </Switch>
             <ButterToast position={{vertical:POS_TOP,horizontal:POS_RIGHT}}/>
           </div>
@@ -34,8 +31,8 @@ class App extends React.Component {
           <div className="App">
             <Nav />
             <Switch>
-              <Route path="/login" component={ Login }></Route>
-              <Route path="/register" component={ Register }></Route>
+              <Route path="/productList" component={ ProductList }></Route>
+              <Route path="/" component={ ProductList }></Route>
             </Switch>
             <ButterToast position={{vertical:POS_TOP,horizontal:POS_RIGHT}}/>
           </div>
