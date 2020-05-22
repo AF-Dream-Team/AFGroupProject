7875f3d9180a773ee272b911e6b9a277cec434f6
ranruwini <ranruwini@gmail.com>
2020-05-22 08:29:01 +0530

    updated files and added comments

--- stack-api/controllers/messageController.js
+++ stack-api/controllers/messageController.js
@@ -5,6 +5,7 @@ var ObjectID= require('mongoose').Types.ObjectId
 
 var { message } = require('../models/message')
 
+// to retrieve all records
 router.get('/',(req,res)=>{
     message.find((err,docs)=>{
         if(!err){
@@ -15,6 +16,7 @@ router.get('/',(req,res)=>{
     })
 })
 
+// to publish a message with product,name and email
 router.post('/',(req,res)=>{
     var newRecord= new message({
         product: req.body.product,
@@ -32,6 +34,7 @@ router.post('/',(req,res)=>{
     })
 })
 
+//to update the message content
 router.put('/:id',(req,res)=>{
     if(!ObjectID.isValid(req.params.id)){
         return res.status(400).send('No record with given id : '+req.params.id)
@@ -50,6 +53,7 @@ router.put('/:id',(req,res)=>{
     })
 })
 
+// to delete a message
 router.delete('/:id',(req,res)=>{
     if(!ObjectID.isValid(req.params.id)){
         return res.status(400).send('No record with given id : '+req.params.id)
--- stack-client/src/components/oneProduct.js
+++ stack-client/src/components/oneProduct.js
@@ -10,7 +10,20 @@ const initialState = {
     comment: "",
     confirmButton: "Send",
     commentError: "",
-    commentId: ""
+    commentId: "",
+	wishlist:[],
+    wishError:"",
+    wish_list:"",
+    proId: "",
+    proName: "",
+    proPrice: "",
+    proDiscount: "",
+    proQuantity : "",
+    proImage : "",
+    quantity : "",
+    type:"",
+    total:"",
+    quantityError:""
 }
 
 class oneProduct extends React.Component {
@@ -49,7 +62,118 @@ class oneProduct extends React.Component {
         .then(json => {const msg = json.filter(msg => msg.product===localStorage.getItem('itemId'))
             this.setState({messages: msg})
         })
+		const wurl = "http://localhost:4000/wishlist";
+        fetch(wurl).then(response => response.json())
+        .then(json => {const wish = json.filter(wish => wish.email===localStorage.getItem('userEmail'))
+            this.setState({wishlist: wish})
+        })
     }
+// adding an item to the wishlist
+	    onWish(){
+        if(this.state.wish_list){
+            if(this.state.quantity){
+                if(this.state.quantity<=this.state.proQuantity){
+                    const total=this.state.quantity*this.state.proPrice-(this.state.proDiscount*this.state.quantity)
+                    this.setState({ type:'wishlist' ,total:total , email: localStorage.getItem('userEmail') }, () => { 
+                        api.myItem().create(this.state)
+                        .then(res =>{
+                            ButterToast.raise({
+                                content: <Cinnamon.Crisp title="Online Store"
+                                    content="Wishlist Product Add Successful!"
+                                    scheme={Cinnamon.Crisp.SCHEME_PURPLE}
+                                    icon={<AssignmentTurnedIn />}
+                                />
+                            })
+                            this.componentDidMount()
+                            this.setState(initialState)
+                        })
+                    })
+                }else{
+                    this.setState({quantityError:"Quantity Error!"})
+                }
+            }else{
+                this.setState({quantityError:"Quantity Required!"})
+            }
+            this.setState({wishError:""})
+        }else{
+            this.setState({wishError:"Plaese select"})
+        }
+    }
+	
+	// updating a comment given for a product
+	    editButton(id,msg,email){
+        if(localStorage.getItem('userEmail')){
+            if(email===localStorage.getItem('userEmail')){
+                return  [<button type='button' onClick={() => this.onChange(id,msg)} class='btn btn-success'>EDIT</button>,
+                        <button type='button' onClick={() => this.onDelete(id)} class='btn btn-danger'>Delete</button>];
+            }
+        }
+    }
+
+	// deleting a comment given for a product
+    onDelete(id){
+        if (window.confirm("Are you sure to delete comment?")) {
+            api.message().delete(id)
+            .then(res =>{
+                ButterToast.raise({
+                    content: <Cinnamon.Crisp title="Online Store"
+                        content="Delete Successful!"
+                        scheme={Cinnamon.Crisp.SCHEME_PURPLE}
+                        icon={<AssignmentTurnedIn />}
+                    />
+                })
+                this.componentDidMount()
+            });
+        }
+    }
+
+	// adding a comment for a product
+    handleSubmit = e => {
+        e.preventDefault();
+        const isValid = this.validate();
+        if(isValid){
+            if(localStorage.getItem('userEmail')){
+                const data ={ message:this.state.comment,name:localStorage.getItem('userName'),email:localStorage.getItem('userEmail'),product:localStorage.getItem('itemId') }
+                if(this.state.commentId){
+                    api.message().update(this.state.commentId,{message:this.state.comment})
+                    .then(res =>{
+                        ButterToast.raise({
+                            content: <Cinnamon.Crisp title="Online Store"
+                                content="Change Successful!"
+                                scheme={Cinnamon.Crisp.SCHEME_PURPLE}
+                                icon={<AssignmentTurnedIn />}
+                            />
+                        })
+                        this.setState(initialState)
+                        this.componentDidMount()
+                    })
+                }else{
+                    api.message().create(data)
+                    .then(res => {
+                        ButterToast.raise({
+                            content: <Cinnamon.Crisp title="Online Store"
+                                content="comment add successfully"
+                                scheme={Cinnamon.Crisp.SCHEME_PURPLE}
+                                icon={<AssignmentTurnedIn />}
+                            />
+                        })
+                        this.setState(initialState)
+                        this.componentDidMount()
+                    } );
+                }
+                
+            }else{
+                ButterToast.raise({
+                    content: <Cinnamon.Crisp title="Online Store"
+                        content="Please Login to the system!"
+                        scheme={Cinnamon.Crisp.SCHEME_PURPLE}
+                        icon={<ExtensionSharp />}
+                    />
+                });
+            }
+        }
+    }
+
 
     onBuy(id){
         localStorage.setItem("itemId",id);
@@ -108,10 +232,26 @@ class oneProduct extends React.Component {
         }
     }
 
-    
+	//validating a comment before adding it
+     validate = () => {
+        let commentError = "";
+
+        if(!this.state.comment){
+            commentError="message Cannot Be Blank"
+        }
+
+        if(commentError){
+            this.setState({ commentError });
+            return false;
+        }else{
+            this.setState({ commentError });
+        }
+
+        return true;
+    }
 
     render (){
-        const { products , messages } = this.state;
+         const { products , messages , wishlist } = this.state;
         return (
             <div class="container">
             <br></br><br></br>
@@ -129,13 +269,32 @@ class oneProduct extends React.Component {
                                             <td class="tableTh" width="65%"><h3>{ product.name  }</h3><br/><h5>category :{ product.name  } / Price: Rs. { product.price }</h5>
                                             <br/><h5>Discount : Rs. { product.discount }</h5>
                                             <br/><h5>Available Quantity : { product.quantity }</h5>
-                                            <br/>Quantity : <input type='text' style={{width: "50px"}} />/{ product.quantity }<br/>
-                                            <br/><button type='button' onClick={() => this.onBuy(product._id)} class='btn btn-success'>BUY</button>
+                                            <br/>Quantity : <input type='text' name="quantity" value={this.state.quantity} onChange={this.handleChange} style={{width: "50px"}} /> / { product.quantity }
+                                            <div style={{color : "red"}}>{this.state.quantityError}</div><br/>
+                                            <br/><button type='button' onClick={() => this.onCart()} class='btn btn-success'>Add to Cart</button>
                                             </td>
                                         </tr>
                                     )}
                                     </tbody>
                                 </table>
+                                <hr/>
+                                <div class="form-group row">
+                                    <label class="col-md-4 col-form-label text-md-right">Your Wishlist</label>
+                                    <div class="col-md-6">
+                                        <select class="form-control" name="wish_list" value={this.state.wish_list} onChange={this.handleChange}>
+                                            <option value="">~select~</option>
+                                            {
+                                                wishlist.map((wishlist) =>
+                                                <option value={ wishlist._id }> { wishlist.name } </option>
+                                            )}
+                                        </select>
+                                        <div style={{color : "red"}}>{this.state.wishError}</div>
+                                    </div>
+                                </div>
+
+                                <div class="col-md-4 offset-md-4">
+                                    <input type="button" class="btn btn-primary" value="Add to List" onClick={() => this.onWish()}/>
+                                </div> 
                                 <hr/>
                                 <form autoComplete="off" onSubmit={this.handleSubmit}>
                                     <div class="form-group row">
@@ -151,7 +310,19 @@ class oneProduct extends React.Component {
                                         <input type="button" class="btn btn-danger" value="Clear" onClick={() => this.onClear()} />
                                     </div>
                                 </form>
-                               
+                                <hr/>
+                                <table>
+                                    {
+                                        messages.map((msg) =>
+
+                                        <tr>
+                                            <td width="150px"><img width="100px" alt="" src="user.png"/></td>
+                                            <td><h5>{ msg.name }</h5>
+                                            <br/><h7>{ msg.message }</h7></td>
+                                            <td width="200px">{this.editButton(msg._id,msg.message,msg.email)}</td>
+                                        </tr>
+                                    )}
+                                </table>
                             </div>
                         </div>
                     </div>
