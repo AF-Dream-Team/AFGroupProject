6e24279c6fe4074e32ada2888a58d261ac67fe50
unknown <tharushihimasharubasinghe@gmail.com>
2020-05-24 10:13:46 +0530

    Update myItem in server side

---
+++ stack-api/models/myItem.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose')
+
+var myItem = mongoose.model('myItem',{
+    type: {type:String},
+    email: {type:String},
+    proId: {type:String},
+    listId: {type:String},
+    proName: {type:String},
+    proPrice: {type:Number},
+    proDiscount: {type:Number},
+    proQuantity : {type:Number},
+    total : {type:Number},
+    proImage : {type:String},
+    payment: {type:String}
+})
+
+module.exports = { myItem }
